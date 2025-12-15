dcc0a4ab5edb38d79a4a391734076943148ce27f
anik33 <anik.cse.nu@gmail.com>
2025-12-15 12:38:30 +0600

    Basic setup

---
+++ src/Router/Routes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../layout/RootLayout";
+import Home from "../Pages/Home/Home";
+
+
+
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:RootLayout,
+     children: [
+      {
+        index: true,
+        Component: Home
+      },
+      
+
+    ]
+    
+  },
+]);
